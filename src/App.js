766bb16265f73959c4b0e79e13e02c5f4b15f75b
Bishal Udash <supertramp.bishal@gmail.com>
2020-03-15 16:13:31 +0545

    inventory manager initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Navbar from "./components/layouts/Navbar";
 
-function App() {
+// import styles
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
+const App = () => {
+  useEffect(() => {
+    M.AutoInit();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      hello
     </div>
   );
-}
+};
 
 export default App;
